abacf231131f3fdeaf13956fde175c989ac690bf
Joshua Sample <josh.sample@ware2go.com>
2023-10-18 00:41:49 -0400

    sidebar footer updated

--- components/AppLayout/AppLayout.tsx
+++ components/AppLayout/AppLayout.tsx
@@ -1,4 +1,10 @@
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
+import Link from "next/link";
+
 export const AppLayout = ({ children}: { children: React.ReactNode }) => {
+  const { user } = useUser();
+  console.log('USER: ', user);
   return (
     <div className="grid grid-cols-[18.75rem_1fr] h-screen max-h-screen">
       <div className="flex flex-col text-white overflow-hidden">
@@ -10,7 +16,29 @@ export const AppLayout = ({ children}: { children: React.ReactNode }) => {
         <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
           list of posts
         </div>
-        <div className="bg-cyan-800">user information</div>
+        <div className="flex bg-cyan-800 items-center gap-2 border-t border-t-black/50 h-20 px-2">
+        {!!user ? (
+          <>
+            <div className="min-w-[50px]">
+              <Image
+                src={user.picture ?? ''}
+                alt={user.name ?? ''}
+                height={50}
+                width={50}
+                className="rounded-full"
+              />
+              
+              
+            </div>
+            <div className="flex-1">
+              <div className="font-bold">{user.nickname ?? ''}</div>
+              <Link className="text-sm" href="/api/auth/logout">Logout</Link>
+            </div>
+          </>
+        ) : (
+          <Link href="/api/auth/login">Login</Link>
+        )}
+        </div>
       </div>
       <div>
         {children}
